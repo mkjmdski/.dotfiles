4ffaa26100968f3b1ec94755b6080a7c45157d04
Mikołaj Młodzikowski <mmlodzikowski@apptension.com>
2018-08-02 11:47:21 +0200

    Add some vscode extensions

--- provisioning/vscode/install.sh
+++ provisioning/vscode/install.sh
@@ -4,10 +4,10 @@ function set_config_path {
     unameOut="$(uname -s)"
     case "${unameOut}" in
         Linux*)
-            echo "$HOME/.config/Code/User/"
+            echo "$HOME/.config/Code/User"
         ;;
         Darwin*)
-            echo "$HOME/Library/Application Support/Code/User/"
+            echo "$HOME/Library/Application Support/Code/User"
         ;;
         *) echo "UNKOWN OS: ${unameOut}" && exit -1
     esac
@@ -15,11 +15,11 @@ function set_config_path {
 
 function link_config {
     config_path="$(set_config_path)"
-    rm "${config_path}/settings.json"
     currDir=$PWD
     (
         cd "${config_path}"
         for conf_file in settings.json snippets; do
+            rm -rf "${config_path}/${conf_file}"
             ln -s "${currDir}/${conf_file}" "${conf_file}"
         done
     )
@@ -29,7 +29,6 @@ function install_extensions {
     extensions=(
         bungcip.better-toml
         marcostazi.VS-code-vagrantfile
-        mauve.terraform
         PeterJausovec.vscode-docker
         redhat.vscode-yaml
         robertohuertasm.vscode-icons
@@ -40,11 +39,19 @@ function install_extensions {
         tinkertrain.theme-panda
         wholroyd.jinja
         dunstontc.viml
-
+        loganarnett.tf-snippets
+        erd0s.terraform-autocomplete
+        mauve.terraform
+        mindginative.terraform-snippets
+        mrmlnc.vscode-apache
     )
-    for extension in extensions; do
-        code --install-extension $extension
+    installed_extensions="$(code --list-extensions)"
+    for extension in $extensions; do
+        if [[ ! $installed_extensions = *"$extension"* ]]; then
+            code --install-extension $extension
+        fi
     done
 }
 
 link_config
+install_extensions
