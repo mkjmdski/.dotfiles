497815f9e0573e4b3ded6d943ecf5910f71f9ffc
mkjmdski <mikolaj.mlodzikowski@gmail.com>
2018-08-14 00:19:54 +0200

    Add tty setup

--- programs/terminal.sh
+++ programs/terminal.sh
@@ -1,1 +1,21 @@
-# brew cask install iterm2
+#!/bin/bash
+
+function main {
+    os="$(get_os)"
+    case "${os}" in
+        CentOS*)
+            install_package_centos terminator
+        ;;
+        Ubuntu*)
+            install_package_ubuntu terminator
+        ;;
+        Darwin*)
+            brew cask install iterm2
+        ;;
+        *)
+            echo "Operating system > ${os} < is not supported" && exit 1
+        ;;
+    esac
+}
+
+main
