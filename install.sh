6c64f5040e6dc1b5427519c43ca3a48e16c86bc3
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2018-08-29 15:55:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@
     cd $HOME
     git clone --depth=1 https://github.com/mkjmdski/.dotfiles.git
     cd .dotfiles
-    source $(git rev-parse --show-toplevel)/zsh/install.sh
-    for file in $(git rev-parse --show-toplevel)/{.hooks,git,tmux,vim,vscode}/install.sh; do
-        source "${file}"
+    for directory in zsh .hooks git tmux vim vscode; do
+        (
+            cd $directory
+            ./install.sh
+        )
     done
 )
