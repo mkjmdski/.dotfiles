29169c23320594817f21fbb273d8e77f51fb610d
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2021-07-28 22:05:52 +0200

    fzf adjust

--- zsh/sources/zinit.zsh
+++ zsh/sources/zinit.zsh
@@ -12,7 +12,21 @@ for binary in fasd kubectl; do
 done
 
 zinit ice from"gh-r" as"program"
-zinit load junegunn/fzf-bin
+zinit load junegunn/fzf
+# zinit light bonnefoa/kubectl-fzf
+
+# Use fd (https://github.com/sharkdp/fd) instead of the default find
+# command for listing path candidates.
+# - The first argument to the function ($1) is the base path to start traversal
+# - See the source code (completion.{bash,zsh}) for the details.
+_fzf_compgen_path() {
+  fd --hidden --follow --exclude ".git" . "$1"
+}
+# Use fd to generate the list for directory completion
+_fzf_compgen_dir() {
+  fd --type d --hidden --follow --exclude ".git" . "$1"
+}
+
 
 for plugin in extract command-not-found gpg-agent last-working-dir colored-man-pages copydir zsh-interactive-cd; do
     zinit snippet OMZ::plugins/$plugin/$plugin.plugin.zsh
