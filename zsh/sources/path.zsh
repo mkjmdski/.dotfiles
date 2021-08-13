0f62845e2b9c1e515f4798560d2348579676358f
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2021-08-13 15:59:07 +0200

    add new nvm loading script

--- zsh/sources/path.zsh
+++ zsh/sources/path.zsh
@@ -47,8 +47,11 @@ if [ ! "$PATH_LOADED" = "true" ]; then
     export PATH_LOADED="true"
     export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 
-    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+    [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
+
     if [[ $commands[jira] ]]; then
         eval "$(jira --completion-script-zsh)"
     fi
