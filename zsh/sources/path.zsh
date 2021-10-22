376928a3e819bd877cfaff6eb8b3455a7642612f
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2021-10-22 13:34:00 +0200

    fix nvm

--- zsh/sources/path.zsh
+++ zsh/sources/path.zsh
@@ -51,8 +51,9 @@ if [ ! "$PATH_LOADED" = "true" ]; then
 
 
     export NVM_DIR="$HOME/.nvm"
-    [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-    [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 
     if [[ $commands[jira] ]]; then
         eval "$(jira --completion-script-zsh)"
