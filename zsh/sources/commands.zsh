ed15eb10261da2ad4d793af8c552a5df6d450e82
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2022-09-07 17:39:33 +0200

    add azure autocomplete

--- zsh/sources/commands.zsh
+++ zsh/sources/commands.zsh
@@ -179,3 +179,28 @@ if [[ $commands[gh] ]]; then
         eval "gh pr create $args --title='$title' --body='$body' --reviewer='$reviewer'"
     }
 fi
+
+# az cli autocomplete
+if [ "$DOTFILES_CONF_azure" = "true" ]; then
+    _python_argcomplete() {
+        local IFS=$'\013'
+        local SUPPRESS_SPACE=0
+        if compopt +o nospace 2> /dev/null; then
+            SUPPRESS_SPACE=1
+        fi
+        COMPREPLY=( $(IFS="$IFS" \
+                    COMP_LINE="$COMP_LINE" \
+                    COMP_POINT="$COMP_POINT" \
+                    COMP_TYPE="$COMP_TYPE" \
+                    _ARGCOMPLETE_COMP_WORDBREAKS="$COMP_WORDBREAKS" \
+                    _ARGCOMPLETE=1 \
+                    _ARGCOMPLETE_SUPPRESS_SPACE=$SUPPRESS_SPACE \
+                    "$1" 8>&1 9>&2 1>/dev/null 2>/dev/null) )
+        if [[ $? != 0 ]]; then
+            unset COMPREPLY
+        elif [[ $SUPPRESS_SPACE == 1 ]] && [[ "$COMPREPLY" =~ [=/:]$ ]]; then
+            compopt -o nospace
+        fi
+    }
+    complete -o nospace -o default -o bashdefault -F _python_argcomplete "az"
+fi
