9ec5e94f09450dfafb4e71c41cabe61135fae836
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2023-05-29 12:20:52 +0200

    feat: fix git

--- zsh/sources/spaceship.zsh
+++ zsh/sources/spaceship.zsh
@@ -9,13 +9,13 @@ SPACESHIP_GIT_LAST_COMMIT_PREFIX="${SPACESHIP_GIT_LAST_COMMIT_PREFIX="("}"
 SPACESHIP_GIT_LAST_COMMIT_SUFFIX="${SPACESHIP_GIT_LAST_COMMIT_SUFFIX=") "}"
 SPACESHIP_GIT_LAST_COMMIT_COLOR="${SPACESHIP_GIT_LAST_COMMIT_COLOR="magenta"}"
 
-# ------------------------------------------------------------------------------
-# Section
-# ------------------------------------------------------------------------------
+# # ------------------------------------------------------------------------------
+# # Section
+# # ------------------------------------------------------------------------------
 
-# Show git_last_commit status
-# spaceship_ prefix before section's name is required!
-# Otherwise this section won't be loaded.
+# # Show git_last_commit status
+# # spaceship_ prefix before section's name is required!
+# # Otherwise this section won't be loaded.
 spaceship_git_last_commit() {
   # If SPACESHIP_GIT_LAST_COMMIT_SHOW is false, don't show git_last_commit section
   [[ $SPACESHIP_GIT_LAST_COMMIT_SHOW == false ]] && return
@@ -28,15 +28,10 @@ spaceship_git_last_commit() {
   # last commit in the current direcotry
   git_last_commit_status=$(git show --pretty='format:%s|%cr' $(git rev-list -1 HEAD -- .) 2>/dev/null | head -n 1)
 
-  # Exit section if variable is empty
-  [[ -z $git_last_commit_status ]] && return
-
   # Display git_last_commit section
-  spaceship::section::v3 \
+  spaceship::section \
     "$SPACESHIP_GIT_LAST_COMMIT_COLOR" \
-    "$SPACESHIP_GIT_LAST_COMMIT_PREFIX" \
-    "$SPACESHIP_GIT_LAST_COMMIT_SYMBOL$git_last_commit_status" \
-    "$SPACESHIP_GIT_LAST_COMMIT_SUFFIX"
+    "${SPACESHIP_GIT_LAST_COMMIT_PREFIX}${SPACESHIP_GIT_LAST_COMMIT_SYMBOL}${git_last_commit_status}${SPACESHIP_GIT_LAST_COMMIT_SUFFIX}"
 
 }
 
